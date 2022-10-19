f7ac84e34fe75732edfb39997c0d4f77af749f42
theConsummateProfessional <ehood2@una.edu>
2022-10-19 01:06:19 -0500

    Changed up things in attempt to fix key issue... not successful yet

--- frontend/src/components/hero.tsx
+++ frontend/src/components/hero.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
+import {useState} from 'react';
+
 export default function Hero(props: any) {
-    console.log(props.top)
+    const [key, setKey] = useState<any>('');
+
+    const handler = (event: any) => {
+        console.log(event.key)
+        setKey(event.key);
+    }
+
     return (
         <div style={{
             width: '25px',
             height: '25px',
             backgroundColor: 'black',
-            top: props.top,
+            top: '50%',
             borderRadius: '50%'
         }}></div>
     )
--- frontend/src/pages/Game.tsx
+++ frontend/src/pages/Game.tsx
@@ -1,24 +1,8 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
 
 import Hero from '../components/hero'
 
-import useKeyPress from "../hooks/useKeyPress";
-
 export default function Game(props: any) {
-    let keyPress: string = useKeyPress();
-    console.log(keyPress);
-    const [top, setTop] = useState(50)
-
-    useEffect(() => {
-        console.log("In here")
-        if(keyPress === 'w') {
-            setTop(top + 1);
-            console.log("hello")
-        }
-    }, [keyPress])
-
-
     return (
         <>
             <div style={{
@@ -34,7 +18,7 @@ export default function Game(props: any) {
                     alignItems: 'center',
                     justifyContent: 'center'
                 }}>
-                    <Hero top={top}/>
+                    <Hero />
                 </div>
             </div>
         </>
